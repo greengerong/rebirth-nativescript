8155c9f47090cf3e85eddd8273d48fba0752ad9f
polang <grzx2210@163.com>
2016-12-02 20:54:10 +0800

    [feature]  card view

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,17 +3,17 @@ import { ObservableArray } from 'data/observable-array';
 import { SetupItemViewArgs } from 'nativescript-angular';
 
 
-
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
 })
 export class AppComponent {
     public countries: ObservableArray<any>;
+    public htmlString: string;
 
     constructor(public elementRef: ElementRef) {
         this.countries = new ObservableArray([{ name: 'rebirth', continent: '-------' }])
-
+        this.htmlString = '<span><h1>HtmlView demo in <font color="blue">NativeScript</font> App</h1></span>';
     }
 
 

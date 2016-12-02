302c27b9180806634e529a62f1c553785b968192
polang <grzx2210@163.com>
2016-12-02 15:26:07 +0800

    [task]  article list on view

--- app/components/article-list/article-list.component.ts
+++ app/components/article-list/article-list.component.ts
@@ -1,45 +1,55 @@
-import { Component, ElementRef } from "@angular/core";
-import { SetupItemViewArgs } from 'nativescript-angular';
-import { ObservableArray } from 'data/observable-array';
-import { ArticleService } from '../../core/article-service/article.service';
+import { Component, ElementRef, OnInit, Output, EventEmitter } from "@angular/core";
+import { ArticleService, Article, SearchResult } from '../../core';
 import { environment } from '../../environments/environment';
-
-class Item {
-    constructor(public name: string) {
-    }
-}
-
-let items = ["ALL Heroes (header)", "Razor", "Rubick", "Phantom Lancer", "Legion Commander", "Brewmaster",
-    "Outworld Devourer", "Sniper", "Lina", "Sven", "Visage", "Undying", "Tiny", "Tidehunter", "Puck", "Ursa",
-    "Magnus", "Earthshaker", "Windrunner", "Techies", "Crystal Maiden", "Batrider", "Riki", "Invoker", "Venomancer",
-    "Timbersaw", "Wraithking", "Anti Mage", "Ancient Apparition", "Troll Warlord", "Lich", "Enchantress",
-    "Bristleback", "Pudge", "(footer)"];
-
 
 @Component({
     selector: "article-list",
     templateUrl: "components/article-list/article-list.component.html",
 })
-export class ArticleListComponent {
-    public dataItems: Array<Item>;
-    public pageIndex = 1;
+export class ArticleListComponent implements OnInit {
+    pageIndex = 1;
+    article: SearchResult<Article>;
+    @Output()
+    articleClicked = new EventEmitter();
 
     constructor(public elementRef: ElementRef, private articleService: ArticleService) {
-        this.dataItems = [];
-        this.articleService.getArticles(this.pageIndex, environment.article.pageSize)
-            .subscribe(
-                t=> console.log(JSON.stringify(t), "========="),
-                error=> console.log(error, "====error=====")
-            );
 
-        for (let i = 0; i < items.length; i++) {
-            this.dataItems.push(new Item(items[i]));
-        }
     }
 
-    onSetupItemView(args: SetupItemViewArgs) {
-        args.view.context.third = (args.index % 3 === 0);
-        args.view.context.header = ((args.index + 1) % items.length === 1);
-        args.view.context.footer = (args.index + 1 === items.length);
+    ngOnInit() {
+        this.pageChange(this.pageIndex);
+    }
+
+    ngOnDestroy(): void {
+        this.articleClicked.complete();
+    }
+
+    gotoPage(url) {
+        this.articleClicked.emit(url);
+    }
+
+    loadMore() {
+        this.pageIndex += 1;
+        this.pageChange(this.pageIndex, () => 1);
+    }
+
+    pageChange(pageIndex, done?: () => void) {
+        this.articleService.getArticles(pageIndex, environment.article.pageSize)
+            .subscribe(result => {
+                    if (!this.article) {
+                        this.article = result;
+                        return;
+                    }
+
+                    this.article.pageIndex = result.pageIndex;
+                    this.article.total = result.total;
+                    this.article.result.push(...result.result);
+
+                    done && done();
+                },
+                (e) => {
+                    console.log(e, 'ArticleListComponent error');
+                    done && done();
+                });
     }
 }

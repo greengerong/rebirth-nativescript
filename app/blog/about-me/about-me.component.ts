a8921fae2b35b5c96c38b93bd421ad76b4b768e6
polang <grzx2210@163.com>
2016-12-04 10:05:20 +0800

    [feature]  css for article

--- app/blog/about-me/about-me.component.ts
+++ app/blog/about-me/about-me.component.ts
@@ -1,7 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Article, ArticleService } from '../../core';
 import { AboutService } from './about.service';
-import { HtmlView } from 'ui/html-view';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import * as fs from "file-system";
 
@@ -12,24 +11,19 @@ import * as fs from "file-system";
 })
 export class AboutMeComponent implements OnInit {
     article: Article;
-    @ViewChild('articleView')
-    articleView: ElementRef;
 
     constructor(private  aboutService: AboutService) {
 
     }
 
     ngOnInit() {
-        console.log("AboutMeComponent AboutMeComponent AboutMeComponent=========");
         this.aboutService.getArticle()
             .subscribe(article => {
                 fromPromise(fs.File.fromPath(__dirname + "/article-item.css")
                     .readText()
                     .then((css) => {
-
                             article.html = article.html + `<style type="text/css">${css}</style>`;
                             this.article = article;
-
                         }
                     ));
             });
